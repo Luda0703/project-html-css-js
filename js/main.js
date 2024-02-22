a0e89c15ff1090fbd915b826ab3d9e5e3c783bfb
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2024-02-22 11:23:35 +0200

    accordeon

--- js/main.js
+++ js/main.js
@@ -37,3 +37,21 @@ const swiperOne = new Swiper('.feedback__slider', {
       }
   });
 
+  const accordeon = document.querySelector('.accordeon');
+  const accordeonTitles = accordeon.querySelectorAll('.accordeon__title');
+
+  accordeonTitles.forEach.call(accordeonTitles, function(accordeonTitle) {
+    accordeonTitle.addEventListener('click', function() {
+        const currentText = accordeonTitle.parentElement.querySelector('.accordeon__text');
+
+        accordeonTitle.classList.toggle('accordeon__title-active');
+        currentText.classList.toggle('accordeon__text-visible');
+
+        if(currentText.classList.contains('accordeon__text-visible')) {
+            currentText.style.maxHeingt = currentText.scrollHeight + 'px';
+        } else {
+            currentText.style.maxHeingt = null;
+        }
+    })
+  })
+
